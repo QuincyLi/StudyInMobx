07581884754daacdb436f9e5b0313b07abed6e10
李权 <466217509@qq.com>
2018-04-26 14:31:09 +0800

    mobx demo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import {
-  observable
-} from "mobx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {observer} from 'mobx-react';
+import appState from './Observables/appState';
 
-class Todo {
-  id = Math.random();
-  @observable title = "";
-  @observable finished = false;
-}
+@observer
+class TimerView extends React.Component {
+    render() {
+        return (<button onClick={this.onReset}>
+                Seconds passed: {this.props.appState.timer}
+            </button>);
+    }
+
+    onReset = () => {
+        this.props.appState.resetTimer();
+    }
+};
+
+ReactDOM.render(<TimerView appState={appState} />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,41 @@
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-
-module: { //我写一个module
-  //配置一个rules(规则),rules是一个数组,里面包含一条一条的规则
-  rules: [{
-      // test 表示测试什么文件类型
-      test: /\.css$/,
-      // 使用 'style-loader','css-loader'
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader', // 回滚
-        use: [{
-            loader: 'css-loader'
-          },
-          {
-            loader: 'postcss-loader'
-          } //利用postcss-loader自动添加css前缀
-        ],
-        publicPath: '../' //解决css背景图的路径问题
-      })
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");  
+const webpack = require('webpack'); // 新增
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            }
+        ]
     },
-    {
-      test: /\.less$/,
-      use: ExtractTextPlugin.extract({ //分离less编译后的css文件
-        fallback: 'style-loader',
-        use: ['css-loader', 'less-loader']
-      })
-    },
-    {
-      test: /\.(sass|scss)$/,
-      use: ['style-loader', 'css-loader', 'sass-loader']
-    },
-    {
-      test: /\.(js|jsx)$/,
-      use: ['babel-loader'],
-      exclude: /node_modules/
-    },
-    {
-      test: /\.(png|jpg|gif)$/,
-      use: [{
-        loader: 'url-loader',
-        options: { // 这里的options选项参数可以定义多大的图片转换为base64
-          limit: 50000, // 表示小于50kb的图片转为base64,大于50kb的是路径
-          outputPath: 'images' //定义输出的图片文件夹
-        }
-      }]
+    devtool: 'source-map',
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new webpack.NamedModulesPlugin(), // 新增
+        new webpack.HotModuleReplacementPlugin() //新增
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 8033,
+        host: "127.0.0.1",
+        hot: true // 新增
     }
-  ]
-}
+};
